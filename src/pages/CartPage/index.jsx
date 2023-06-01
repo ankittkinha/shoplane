f31ac81b117bc9a55ae34511a4aefa3e979dc19d
ankittkinha <ankittkinha@gmail.com>
2023-06-01 21:26:26 +0530

    fixed moving summary while changing size

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -14,13 +14,13 @@ export default function CartPage() {
       <Navbar />
 
       <div className="row">
-      <div className="col-sm-10 mb-3 mb-sm-0">
-        {
-          (Carts.length !== 0) ? Carts.map(product => <CartProduct key={product.id} data={product} />) : <EmptyCart />
-        }
+        <div className="col-sm-10 mb-3 mb-sm-0">
+          {
+            (Carts.length !== 0) ? Carts.map(product => <CartProduct key={product.id} data={product} />) : <EmptyCart />
+          }
 
         </div>
-        <div className="col-sm-2">
+        <div className="col-sm-2 summary-div">
           <div className="card sticky-summary">
             <CartSummary data={Carts} />
           </div>
